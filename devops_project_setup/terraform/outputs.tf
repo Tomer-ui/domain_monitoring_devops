0e8eea5ea04b759f8e94626791103f820a547202
tomer dachbash <tomasds123456@gmail.com>
2025-12-01 23:16:50 +0200

    fixed playbook + jcasc

--- devops_project_setup/terraform/outputs.tf
+++ devops_project_setup/terraform/outputs.tf
@@ -1,7 +1,7 @@
 resource "local_file" "ansible_inventory" {
   content = <<EOT
 [master]
-${aws_instance.jenkins_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${abspath("../my-key.pem")} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+${aws_instance.jenkins_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${abspath("../my-key.pem")} ansible_ssh_common_args='-o StrictHostKeyChecking=no' private_ip=${aws_instance.jenkins_master.private_ip}
 
 [node]
 ${aws_instance.jenkins_node.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${abspath("../my-key.pem")} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
